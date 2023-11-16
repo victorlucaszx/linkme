78c9661d2a5aca9ead2d2804faf509d84f1d402c
victorlucaszx <viktorlucas13@gmail.com>
2023-11-16 08:54:19 -0300

    alteraçoes para futuro node no projeto

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 function toggleMode() {
   const html = document.documentElement // pegando o html
-  html.classList.toggle("ligth-mode") // trocando a class
   const img = document.querySelector("#profile img") // query  pesquisa pelo seletor
+
+  html.classList.toggle("ligth-mode") // trocando a class
+
   if (html.classList.contains("ligth-mode")) {
-    // html.classList.contains.remove("dark-mode")
     img.setAttribute("src", "./assets/avatar2.jpg")
+    // Salvando a preferência do usuário no localStorage
+    localStorage.setItem("mode", "light")
   } else {
     img.setAttribute("src", "./assets/avatar.jpg")
     //  html.classList.add("ligth-mode")
+    // Salvando a preferência do usuário no localStorage
+    localStorage.classList("mode", "dark")
   }
 }
+// Verificando se há uma preferência salva no localStorage ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+  const savedMode = localStorage.classList("mode")
+  if (savedMode === "light") {
+    html.classList.contains("ligth-mode")
+    // Definindo a imagem correta de acordo com o modo salvo
+    document
+      .querySelector("#profile img")
+      .setAttribute("src", "./assets/avatar2.jpg")
+  } else {
+    html.classList.contains("dark-mode")
+    // Definindo a imagem correta de acordo com o modo salvo
+    document
+      .querySelector("#profile img")
+      .setAttribute("src", "./assets/avatar.jpg")
+  }
+})
